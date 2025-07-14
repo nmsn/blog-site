7179d6330b5c2e1a092b4a6c4bc44cb1333e95a1
nmsn <136696700@qq.com>
2025-07-14 21:24:14 +0800

    feat: 新增首页

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,27 @@
 import Logo from '@/data/logo-white.svg'
 import Header from './Header'
 import Footer from './Footer'
+import Link from '@/components/Link'
+import { WobbleContent } from '@/components/ui/WobbleContent'
 
 export default async function Page() {
   return (
-    <header className="flex h-dvh w-full flex-col items-center justify-between">
-      <Header />
-      <main>
-        <Logo />
-        <nav>Link</nav>
+    <div className="flex h-dvh w-full flex-col items-center justify-between">
+      <main className="flex h-[90vh] w-full flex-col items-center">
+        <Header />
+        <WobbleContent containerClassName="max-w-[1024px]">
+          <div>
+            <Logo />
+          </div>
+        </WobbleContent>
+        <nav className="flex translate-y-[-100px] justify-between gap-x-20">
+          <Link href="/blog">BLOG</Link>
+          <Link href="/projects">PROJECTS</Link>
+          <Link href="/travel">TRAVEL</Link>
+          <Link href="/about">ABOUT</Link>
+        </nav>
       </main>
       <Footer />
-    </header>
+    </div>
   )
 }
---
+++ components/ui/WobbleContent.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { useState } from 'react'
+import { motion } from 'motion/react'
+import { cn } from '@/lib/utils'
+
+export const WobbleContent = ({
+  children,
+  containerClassName,
+  className,
+}: {
+  children: React.ReactNode
+  containerClassName?: string
+  className?: string
+}) => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  const [isHovering, setIsHovering] = useState(false)
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
+    const { clientX, clientY } = event
+    const rect = event.currentTarget.getBoundingClientRect()
+    const x = (clientX - (rect.left + rect.width / 2)) / 20
+    const y = (clientY - (rect.top + rect.height / 2)) / 20
+    setMousePosition({ x, y })
+  }
+  return (
+    <motion.section
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => {
+        setIsHovering(false)
+        setMousePosition({ x: 0, y: 0 })
+      }}
+      style={{
+        transform: isHovering
+          ? `translate3d(${mousePosition.x}px, ${mousePosition.y}px, 0) scale3d(1, 1, 1)`
+          : 'translate3d(0px, 0px, 0) scale3d(1, 1, 1)',
+        transition: 'transform 0.1s ease-out',
+      }}
+      className={cn('relative mx-auto w-full overflow-hidden rounded-2xl', containerClassName)}
+    >
+      {children}
+    </motion.section>
+  )
+}
